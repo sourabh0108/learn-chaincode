1710a739a4230e5505aa7a1379ce9d5e0ee1ac38
Saurabh Jain <Sourabh.J@synapse.com>
2017-05-02 11:41:20 +0530

    updated for patientDoctorApp

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 
@@ -41,7 +42,7 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
 
-	err := stub.PutState("hello_world", []byte(args[0]))
+	err := stub.PutState("patientDoctorApp", []byte(args[0]))
 	if err != nil {
 		return nil, err
 	}
@@ -76,15 +77,14 @@ func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string)
 		return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the key and value to set")
 	}
 
-	key = args[0] //rename for fun
-	value = args[1]
+	key = args[0]   // patient id
+	value = args[1] // list of accessRecords
 
 	username, _ := GetCertAttribute(stub, "username")
-	role, _ := GetCertAttribute(stub, "role")
-	if role == "Patient" {
+	if username == "user_type4_0" || username == "user_type4_2" {
 		err = stub.PutState(key, []byte(value)) //write the variable into the chaincode state
 	} else {
-		return nil, errors.New(username + " with role " + role + " does not have access to create a patient asset")
+		return nil, errors.New(username + " does not have access to create a patient asset")
 	}
 
 	if err != nil {
@@ -106,13 +106,13 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	return nil, errors.New("Received unknown function query: " + function)
 }
 
-//read method is used to read from the wrold state and return the asset
+//read method is used to read from the wrold state and return the patient accessRecords asset. Arguments to be passed patient and doctor id
 func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	var key, jsonResp string
 	var err error
 
 	if len(args) != 1 {
-		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+		return nil, errors.New("Incorrect number of arguments. Expecting patientID")
 	}
 
 	key = args[0]
@@ -120,6 +120,39 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 	if err != nil {
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 		return nil, errors.New(jsonResp)
+	}
+
+	if key != "patientDoctorApp" {
+
+		doctorUsername, _ := GetCertAttribute(stub, "username")
+		var accessRecords []AccessRecord
+		var newAccessRecordList []AccessRecord
+		var index int
+		index = 0
+		err = json.Unmarshal(valAsbytes, &accessRecords)
+		if err != nil {
+			jsonResp = "{\"Error\":\"Failed to convert byteArray to accessRecords list.\"}"
+			return nil, errors.New(jsonResp)
+		}
+
+		//This loop checks if the logged in doctor has access to any of patient's shared EMRs
+		for _, accessRecord := range accessRecords {
+			if doctorUsername == accessRecord.DoctorID {
+				newAccessRecordList[index] = accessRecord
+			}
+		}
+
+		if len(newAccessRecordList) < 1 {
+			jsonResp = "{\"Error\":\"You dont have access to any EMR of Patient " + key + "\"}"
+			return nil, errors.New(jsonResp)
+		}
+
+		newAccessRecordsAsbytes, err := json.Marshal(&newAccessRecordList)
+		if err != nil {
+			jsonResp = "{\"Error\":\"Failed to convert newAccessRecords to byteArray.\"}"
+			return nil, errors.New(jsonResp)
+		}
+		valAsbytes = newAccessRecordsAsbytes
 	}
 
 	return valAsbytes, nil
